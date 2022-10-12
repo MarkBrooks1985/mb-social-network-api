ae6ff30a44e2154f3e066be5087e770fd1ce0d45
MarkBrooks1985 <alynnnari@gmail.com>
2022-10-12 17:28:45 +1030

    5th commit, another repo update

---
+++ controllers/user-controller.js
@@ -0,0 +1,24 @@
+const { User } = require("../models");
+
+const userController = {
+  createUser(req, res) {
+    User.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+  getUsers(req, res) {
+    User.find()
+      .then(async (users) => {
+        const userObj = {
+          users,
+        };
+        return res.json(userObj);
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+};
+
+module.exports = userController;
--- server.js
+++ server.js
@@ -1,22 +1,15 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const app = express();
+const db = require("./config/connection");
+const routes = require("/routes");
+
 const PORT = process.env.PORT || 3001;
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(require("./routes"));
+app.use(routes);
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/social-network",
-  {
-    useFindAndModify: false,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
-
-mongoose.set("debug", true);
-
-app.listen(PORT, () => console.log(`Listening on localhost:${PORT}`));
+db.once("open", () => {
+  app.listen(PORT, () => console.log(`Listening on localhost:${PORT}`));
+});
