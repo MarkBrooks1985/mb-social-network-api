2ab28f20d5d118bb6ad50907e16d9cb7dcac0216
MarkBrooks1985 <alynnnari@gmail.com>
2022-10-12 18:36:03 +1030

    10th commit, another update

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
-const thoughtRoutes = require("./thought-routes");
 const userRoutes = require("./user-routes");
+const thoughtRoutes = require("./thought-routes");
 
-router.use("/thought", thoughtRoutes);
 router.use("/user", userRoutes);
+router.use("/thought", thoughtRoutes);
 
 module.exports = router;
